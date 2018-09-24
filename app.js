aef586215dc2cd28a0d41da2fc82f5cfe7c92d89
nellson.maia@gmail.com <nellson.maia@gmail.com>
2018-09-24 19:40:56 +0100

    Adding files

---
+++ app.js
@@ -0,0 +1,112 @@
+/**
+ * Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"use strict";
+
+var express = require('express');
+var app = express();
+
+var PORT = 8002;
+
+var session = require('express-session');
+
+//session-related stuff
+var sess = {
+ secret: 'asdasdasdasdasdasdasd',
+ cookie: {},
+ resave: false,
+ saveUninitialized: true
+};
+
+if (app.get('env') === 'production') {
+ sess.cookie.secure = true; // serve secure cookies, requires https
+}
+
+app.use(session(sess));
+
+var Auth0Strategy = require('passport-auth0'),
+    passport = require('passport');
+
+//passport-auth0
+var strategy = new Auth0Strategy({
+  domain: 'delegateadmin.eu.auth0.com',
+  clientID: 'iRFfqb1vW1OecUscvvtb15Nakz01kfBK',
+  clientSecret: 'uI-QTVOM0BzrGsXS0D6otf01QtyKqZbkmQgIB9TP3Enn1REAc8rYSYt6N2WqHbhc', // Replace this with the client secret for your app
+  callbackURL: 'http://localhost:8002/amp-access/login/callback',
+  state: true
+ },
+ function(accessToken, refreshToken, extraParams, profile, done) {
+   // accessToken is the token to call Auth0 API (not needed in the most cases)
+   // extraParams.id_token has the JSON Web Token
+   // profile has all the information from the user
+   return done(null, profile);
+ }
+);
+
+passport.use(strategy);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(function(req, res, next) {
+  res.locals.loggedIn = false;
+  if (req.session.passport && typeof req.session.passport.user !== 'undefined') {
+    res.locals.loggedIn = true;
+  }
+  next();
+});
+
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+app.set('view engine', 'html');
+app.set('partials', {
+  styles: 'styles',
+  material: 'material.css'
+});
+app.enable('view cache');
+app.engine('html', require('hogan-express'));
+app.locals.delimiters = '<% %>';
+
+// Static Content
+app.use(express.static(__dirname + '/public'));
+
+// Middleware 
+app.use(require('./middlewares/amp-access-cors'));
+app.use(require('./middlewares/logging'));
+
+// Controllers
+app.use(require('./controllers'));
+
+var port = process.env.PORT || PORT;
+var server = app.listen(port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Publisher listening on: ', port);
+});
