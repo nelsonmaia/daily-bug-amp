069d395ace1d24cc4443370df892eb44ddf3d7d2
nellson.maia@gmail.com <nellson.maia@gmail.com>
2018-09-25 15:08:41 +0100

    handling error

--- controllers/amp-access/login.js
+++ controllers/amp-access/login.js
@@ -59,7 +59,7 @@ router.get('/',  function(req, res) {
   });
 // Perform the final stage of authentication and redirect to '/user'
 router.get('/callback',
-  passport.authenticate('auth0', { failureRedirect: '/login' }),
+  passport.authenticate('auth0', { failureRedirect: 'error' }),
   function (req, res) {
     if (!req.user) {
       throw new Error('user null');
@@ -91,6 +91,16 @@ router.get('/callback',
 
   }
 );
+
+
+router.get('/error', function(req,res){
+  
+  console.log("error is ");
+  console.log(req);
+
+  res.sendStatus(400);
+
+});
 
 
 router.get('/reset', function (req, res) {
