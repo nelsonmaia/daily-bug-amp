860ac0d6c80f27c995d5b3c77822198e90cfd9ae
nellson.maia@gmail.com <nellson.maia@gmail.com>
2018-09-25 15:47:47 +0100

    passaport

--- controllers/amp-access/login.js
+++ controllers/amp-access/login.js
@@ -59,12 +59,7 @@ router.get('/login',
   });
 // Perform the final stage of authentication and redirect to '/user'
 router.get('/callback',
-  passport.authenticate('auth0', function (err, user, info) {
-
-      console.log("its is here");
-      console.log(err);
-
-  }),
+  passport.authenticate('auth0', { failureRedirect: 'error'}),
   function (req, res) {
     if (!req.user) {
       throw new Error('user null');
