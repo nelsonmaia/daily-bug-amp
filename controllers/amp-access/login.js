f2b4c77eb6eb6c62da56c8be839971dd6bc6d23a
nellson.maia@gmail.com <nellson.maia@gmail.com>
2019-04-04 17:04:33 +0100

    removing connection

--- controllers/amp-access/login.js
+++ controllers/amp-access/login.js
@@ -53,7 +53,7 @@ router.get('/', function (req, res) {
 router.get('/login',
   passport.authenticate('auth0',
       
-    { scope: 'openid email profile', connection: 'stark-cordco-conn'})
+    { scope: 'openid email profile'})
   , function (req, res) {
     console.log("the callback is here?");
     res.redirect("/");
